c9c60834248c4357f6e43824a8e6ca02bcade741
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-02-26 17:00:26 +0100

    add a new interface Channel

---
+++ app/Interface/Channel.ts
@@ -0,0 +1,12 @@
+import {UserFull} from "@/app/Interface/UserFull";
+import {ChannelMessages} from "@/app/Interface/ChannelMessages";
+
+export interface Channel{
+    adminChannelMembers: UserFull
+    channelMembers: UserFull
+    channelMessages: ChannelMessages
+    createdAt: string
+    id:number
+    name:string
+    owner: UserFull
+}

6c3f56fdf4fd7250bb58e539a5d4c9b3188691a5
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-02-26 13:50:40 +0100

    add global constants inside the app

---
+++ app/common/Global-constants.ts
@@ -0,0 +1,5 @@
+export class GlobalConstants{
+    public static baseUrl = "https://slackbis.thibautstachnick.com/api/"
+    public static token = ""
+    public static isLoggedIn = GlobalConstants.token !== ""
+}

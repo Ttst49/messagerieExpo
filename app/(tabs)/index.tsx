3e6b695ab5ba211db9831111fbc54147fa8163b0
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-02-26 10:29:14 +0100

    add a welcome page with axios call for testing purpose

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,27 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import axios from "axios";
+import React, {useState} from 'react';
+
 
 export default function TabOneScreen() {
+
+  const [joke,setJoke] = useState("")
+
+  function callJoke(){
+    axios.get("https://api.chucknorris.io/jokes/random")
+        .then(response=>{
+          setJoke(response.data.value)
+        })
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <p>Coucou</p>
+      <button onClick={callJoke}>CLICK</button>
+      <span>{joke}</span>
     </View>
   );
 }

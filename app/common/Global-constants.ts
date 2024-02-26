2171bd0175945bd6b6e32d40c2020b8b8d8eb53d
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-02-26 14:50:51 +0100

    add method isLoggedIn

--- app/common/Global-constants.ts
+++ app/common/Global-constants.ts
@@ -1,5 +1,8 @@
 export class GlobalConstants{
     public static baseUrl = "https://slackbis.thibautstachnick.com/api/"
     public static token = ""
-    public static isLoggedIn = GlobalConstants.token !== ""
+
+    public static isLoggedIn(){
+        return GlobalConstants.token != "";
+    }
 }

a873d22ef9493eb83dae680f2d275979e4c77b4c
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-02-27 14:35:48 +0100

    remove unused imports

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -2,7 +2,6 @@ import {StyleSheet, Text} from 'react-native';
 
 import { View } from '@/components/Themed';
 import React from 'react';
-import {Link} from "expo-router";
 
 
 export default function Home() {

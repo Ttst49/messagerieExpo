759c4ac903ad224cd733028865ba69a82a8fbf56
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-02-28 09:19:50 +0100

    change interface by adding details

--- app/Interface/Channel.ts
+++ app/Interface/Channel.ts
@@ -2,9 +2,9 @@ import {UserFull} from "@/app/Interface/UserFull";
 import {ChannelMessages} from "@/app/Interface/ChannelMessages";
 
 export interface Channel{
-    adminChannelMembers: UserFull
-    channelMembers: UserFull
-    channelMessages: ChannelMessages
+    adminChannelMembers: UserFull[]
+    channelMembers: UserFull[]
+    channelMessages: ChannelMessages[]
     createdAt: string
     id:number
     name:string

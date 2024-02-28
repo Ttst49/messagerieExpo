e7b08124683ecad4ed0258b25e4faf292b10cfd5
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-02-28 14:00:44 +0100

    add indication

--- app/common/Global-constants.ts
+++ app/common/Global-constants.ts
@@ -2,6 +2,7 @@ import {User} from "@/app/Interface/User";
 
 export class GlobalConstants{
     public static baseUrl = "https://slackbis.thibautstachnick.com/api/"
+    //replace later with secureStore
     public static token = ""
     public static actualUser: User
 

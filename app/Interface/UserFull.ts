d5f436cdfb84afb544ce102fcbcbab2b635f476b
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-03-01 10:29:13 +0100

    add requests and visibility to userFull interface

--- app/Interface/UserFull.ts
+++ app/Interface/UserFull.ts
@@ -1,8 +1,11 @@
 import {User} from "@/app/Interface/User";
+import {Request} from "@/app/Interface/Request"
 
 export interface UserFull{
     id:number
     lastName:string
     name:string
     relatedTo:User
+    requests: Request[]
+    visibility: boolean
 }

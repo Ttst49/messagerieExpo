d05cb299e35b24a3a42d30b8d308652cfc261e93
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-03-01 10:31:05 +0100

    add friends const add getFriends method add template and css

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,23 +1,57 @@
-import {StyleSheet, Text} from 'react-native';
+import {FlatList, StyleSheet, Text} from 'react-native';
 import {View } from '@/components/Themed';
 import {GlobalConstants} from "@/app/common/Global-constants";
 import {useRouter} from "expo-router";
+import React, {useEffect, useState} from "react";
+import {User} from "@/app/Interface/User";
+import axiosHttp from "@/app/auth/interceptor";
+import {Avatar, Button} from "react-native-paper";
+import {Div} from "@expo/html-elements";
 
 export default function profile() {
-
     const navigation = useRouter()
+    const [friends,setFriends]= useState<User[]>([])
+
+    useEffect(() => {
+        setTimeout(()=>{
+            if (!GlobalConstants.isLoggedIn()){
+                navigation.push("/login")
+            }else {
+                getFriends()
+            }
+        },500)
+    }, []);
+
+    function getFriends(){
+        axiosHttp.get(GlobalConstants.baseUrl+"relations/getFriends")
+            .then((response)=>{
+                console.log(response)
+                setFriends(response.data)
+            })
+    }
+
+
     return (
         <View style={styles.container}>
-            {GlobalConstants.isLoggedIn()
-                ?
-                <View>
-                    <Text>Welcome {GlobalConstants.actualUser.username}</Text>
-                </View>
-                :
-                <View>
-                    <Text>Log in before</Text>
-                </View>
-            }
+            <Text style={styles.text}>
+                Profile
+            </Text>
+            <View style={styles.content}>
+                <FlatList
+                    data={friends}
+                    renderItem={({item}:{item:User})=>
+                        <>
+                            <Div style={styles.avatar}>
+                                <Avatar.Text size={48} label={item.username[0].toUpperCase()} />
+                                <Text style={styles.text}>{item.username}</Text>
+                            </Div>
+                        </>
+                    }
+                />
+                <Button icon="account-details" mode="outlined" onPress={() => console.log('Pressed')}>
+                    Voir tous les utilisateurs
+                </Button>
+            </View>
         </View>
     );
 }
@@ -44,5 +78,21 @@ const styles = StyleSheet.create({
         backgroundColor: 'green',
         color: "white"
     },
+    text:{
+        fontSize: 25
+    },
+    avatar:{
+        width: 125,
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around"
+    },
+    content:{
+        flex: 1,
+        marginTop: "15%",
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 
 });

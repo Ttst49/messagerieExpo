ec5a921688933b1c07b503d33d3e7ecbc725d486
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-03-01 14:19:31 +0100

    change template and remove unused css

--- app/(tabs)/channel.tsx
+++ app/(tabs)/channel.tsx
@@ -1,11 +1,11 @@
 import { FlatList, StyleSheet} from 'react-native';
-import { Text, View } from '@/components/Themed';
+import { View } from '@/components/Themed';
 import React, {useEffect, useState} from 'react';
 import axiosHttp from "@/app/auth/interceptor";
 import {GlobalConstants} from "@/app/common/Global-constants";
 import {Channel} from "@/app/Interface/Channel";
-import {Card} from "@gluestack-ui/themed";
-import {Link, useRouter} from "expo-router";
+import {FAB, Button, Card, Text} from "react-native-paper";
+import {useRouter} from "expo-router";
 
 
 export default function channel() {
@@ -42,75 +42,63 @@ export default function channel() {
                     showsVerticalScrollIndicator={false}
                     data={channels}
                     renderItem={({item}:{item:Channel})=>
-                        <Card style={styles.channelCard}>
-                            <Text style={styles.text}>{item.name}</Text>
-                            {item.owner.id == GlobalConstants.actualUser.profile.id
-                                ?
-                                <>
-                                    <Text style={styles.btn} onPress={()=>{removeChannel(item.id)}}>Supprimer channel</Text>
-                                    <Text style={styles.btn} onPress={()=>{navigation.push({pathname:`/channel/edit/${item.id}`})}}>Editer channel</Text>
-                                    <Text style={styles.btn} onPress={()=>{navigation.push({pathname:`/channel/show/${item.id}`})}}>Montrer channel</Text>
-                                </>
-                                :
-                                <>
-                                    <Text style={styles.btn} onPress={()=>{navigation.push({pathname:`/channel/show/${item.id}`})}}>Montrer channel</Text>
-                                </>
-                            }
+                        <Card
+                            onPress={()=>{navigation.push({pathname:`/channel/show/${item.id}`})}}
+                            style={styles.channelCard}
+                        >
+                            <Card.Content>
+                                <Text variant={"titleLarge"}>{item.name}</Text>
+                            </Card.Content>
+                            <Card.Actions>
+                                {item.owner.id == GlobalConstants.actualUser.profile.id
+                                    ?
+                                    <>
+                                        <Button onPress={()=>{removeChannel(item.id)}}>Supprimer channel</Button>
+                                        <Button onPress={()=>{navigation.push({pathname:`/channel/edit/${item.id}`})}}>Editer channel</Button>
+                                    </>
+                                    :
+                                    <>
+                                    </>
+                                }
+                            </Card.Actions>
                         </Card>
                 }
                     keyExtractor={(item)=>item.id.toString()}
                 />
             </Card>
-            <Link style={styles.btnCreate} href={"/channel/create"} >Créer un nouveau channel</Link>
+            <FAB
+                style={styles.btnCreate}
+                onPress={()=>{navigation.push("/channel/create")}}
+                label={"Créer un nouveau channel"}
+                color={"white"}
+                rippleColor={"black"}
+            />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        marginTop: 50,
         flex: 1,
         flexDirection: "column",
         alignItems: 'center',
         justifyContent: 'center',
     },
     channelCard:{
-        width: '100%',
-        margin: 5,
-        borderRadius: 4,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#252525",
+        margin: 15,
     },
     text:{
         color: "white",
         fontSize: 25,
 
     },
-    btn:{
-        margin: 15,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 12,
-        paddingHorizontal: 32,
-        borderRadius: 4,
-        elevation: 3,
-        backgroundColor: 'gray',
-        color: "white",
-    },
+
     btnCreate:{
         margin: 15,
         alignItems: 'center',
         justifyContent: 'center',
-        paddingVertical: 32,
-        paddingHorizontal: 64,
-        borderRadius: 10,
-        elevation: 3,
-        backgroundColor: 'gray',
-        color: "white",
         position: "absolute",
+        backgroundColor: "gray",
         bottom: 10,
     },
     body:{

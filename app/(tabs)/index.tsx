87c0834dd406e515b1d779b3a265629fd6a91f46
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-03-03 13:18:42 +0100

    change home page for better explanation of the project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,9 @@
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet} from 'react-native';
+import {Text} from 'react-native-paper';
 
 import { View } from '@/components/Themed';
 import React from 'react';
+import {A, BR} from "@expo/html-elements";
 
 
 export default function Index() {
@@ -9,7 +11,16 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>bienvenue</Text>
+      <Text variant={"bodyLarge"}>
+        bienvenue dans cette démonstration d'une messagerie
+        en react native avec <A style={styles.link} href={"https://expo.dev"}>expo </A>
+         basée sur l'api libre Slackbis
+        à retrouver <A style={styles.link} href={"https://github.com/Ttst49/slackBis"}>ici</A>
+      </Text>
+      <Text variant={"bodyLarge"}>
+        Ce produit n'a pas été polie ni finalisé pour le moment,
+        des bugs peuvent donc apparaitre.
+      </Text>
     </View>
   );
 }
@@ -19,8 +30,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    display: "flex"
   },
-  text:{
-    textTransform: 'uppercase',
+  link:{
+    color: "skyblue"
   }
 });

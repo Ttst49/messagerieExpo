23e105b6d497b95b648f3d9cba1735d2f413037b
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-03-06 12:02:13 +0100

    add a way to get friends on a load
    add a way to difference between friends and normal users

--- app/user/index.tsx
+++ app/user/index.tsx
@@ -5,11 +5,13 @@ import {GlobalConstants} from "@/app/common/Global-constants";
 import axiosHttp from "@/app/auth/interceptor";
 import {Text, FlatList, View, StyleSheet} from "react-native";
 import {Div} from "@expo/html-elements";
-import {Avatar, FAB} from "react-native-paper";
+import {Avatar, Button, FAB} from "react-native-paper";
+import {Feather} from "@expo/vector-icons";
 
 export default function user() {
     const navigation = useRouter()
     const [users,setUsers]= useState<User[]>([])
+    const [friends,setFriends]=useState<User[]>([])
 
     useEffect(() => {
         setTimeout(()=>{
@@ -17,6 +19,7 @@ export default function user() {
                 navigation.push("/login")
             }else {
                 getAllUsers()
+                getFriends()
             }
         },500)
     }, []);
@@ -29,6 +32,16 @@ export default function user() {
             })
     }
 
+    function getFriends(){
+        axiosHttp.get(GlobalConstants.baseUrl+"relations/getFriends")
+            .then((response)=>{
+                console.log(response)
+                setFriends(response.data)
+            })
+    }
+
+    function sendFriendRequest(id:number){}
+
 
     return (
         <View style={styles.container}>
@@ -40,10 +53,22 @@ export default function user() {
                     data={users}
                     renderItem={({item}:{item:User})=>
                         <>
-                            <Div style={styles.avatar}>
-                                <Avatar.Text size={48} label={item.username[0]} />
-                                <Text style={styles.text}>{item.username}</Text>
-                            </Div>
+                            {friends.some((user)=>user.username==item.username)
+
+                                ?
+                                    <Div style={styles.avatar}>
+                                            <Avatar.Text size={48} label={item.username[0]} />
+                                            <Feather size={32} name={"user-check"} />
+                                            <Text style={styles.text}>{item.username}</Text>
+                                    </Div>
+                                :
+                                <Div style={styles.avatar}>
+                                    <Avatar.Text size={48} label={item.username[0]} />
+                                    <Feather size={32} name={"user-plus"} onLongPress={SendFriendRequest(item.id)} />
+                                    <Text style={styles.text}>{item.username}</Text>
+                                </Div>
+                            }
+
                         </>
                     }
                 />
